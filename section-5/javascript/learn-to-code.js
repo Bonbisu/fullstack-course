13ce5da6bd25a956f3fb8881066a842ce8498f99
bonbisu <fh.costa@yahoo.com.br>
2018-06-04 13:27:29 -0300

    section 5-34
    working with vars and strings, printed in console

--- section-5/javascript/learn-to-code.js
+++ section-5/javascript/learn-to-code.js
@@ -1,6 +1,16 @@
-var name = "Jack";
+//var name = "Jack";
+//var age = 23;
+//
+//var message = "Hi, my name is " + name + " and I am " + age + " years old!";
+//
+//console.log(message);
+
+var firstName = "John";
+var lastName = "JacobJingleHeimerSmith";
+var dateOfBirth = "10-09-82";
 var age = 23;
+var profileImgUrl = "http://coolpicks.com/johnjacob.jpg"
 
-var message = "Hi, my name is " + name + " and I am " + age + " years old!";
+var loginWelcomeMessage = "Welcome, " + firstName + ". Happy " + age + "rd birthday!";
 
-console.log(message);
+console.log(loginWelcomeMessage);
