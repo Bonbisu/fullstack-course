6c0e066006f1852c15099c70acc359345ea4eafa
bonbisu <fh.costa@yahoo.com.br>
2018-06-04 14:41:08 -0300

    section 5-36
    Add some if, else if and else functions

--- section-5/javascript/learn-to-code.js
+++ section-5/javascript/learn-to-code.js
@@ -28,9 +28,17 @@
 //console.log(result);
 
 var myAccountBalance = 300;
-var nikeSBShoes = 73.23;
+var nikeSBShoes = 799.23;
+var coupon = 500;
 
 if (nikeSBShoes <= myAccountBalance) {
-    myAccountBalance -= nikeSBShoes    
+    myAccountBalance -= nikeSBShoes // same as myAccountBalance = myAccountBalance - nikeSBShoes   
     console.log("We just bought some dope shoes!");
+    console.log("Account Balance: " + myAccountBalance);
+} else if (nikeSBShoes-coupon <= myAccountBalance) {
+    console.log("We just bought some dope shoes with a coupon!");
+    myAccountBalance -= nikeSBShoes - coupon;
+    console.log("Account Balance: " + myAccountBalance);
+} else {
+    console.log("You too broke for shoes bra!");
 }
