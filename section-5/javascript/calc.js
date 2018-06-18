c7dc757ff974bc51bda01c17257b9bbb9396008f
bomber <fh.costa@yahoo.com.br>
2018-06-18 20:15:03 -0300

    section 5-45
    adding a event listener to a submit button form

--- section-5/javascript/calc.js
+++ section-5/javascript/calc.js
@@ -1,3 +1,8 @@
 var numField1 = document.getElementById('numField1');
 var numField2 = document.getElementById('numField2');
 var resultField = document.getElementById('resultField');
+var form = document.getElementById('xIsWhatPercentageOfY');
+
+form.addEventListener('submit', function() {
+    alert("We clickeyclacked something!");
+});
