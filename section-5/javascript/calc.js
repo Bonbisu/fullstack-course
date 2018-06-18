c495bfa803417da96043bcec8f40c0d933686a81
bomber <fh.costa@yahoo.com.br>
2018-06-18 01:07:18 -0300

    organizing

--- section-5/javascript/calc.js
+++ section-5/javascript/calc.js
@@ -1,6 +1,3 @@
 var numField1 = document.getElementById('numField1');
 var numField2 = document.getElementById('numField2');
 var resultField = document.getElementById('resultField');
-
-numField1.value = "TEST"; 
-resultField.innerText = "TEST2";
