dd265979dbf040d19f0068feab3d4985504cc8d5
bomber <fh.costa@yahoo.com.br>
2018-06-18 00:16:11 -0300

    section 5-44
    For inputs, use . value to change the default. For headers, paragraph and tags that have innet text, use .innerText to change

--- section-5/javascript/calc.js
+++ section-5/javascript/calc.js
@@ -1,3 +1,6 @@
 var numField1 = document.getElementById('numField1');
+var numField2 = document.getElementById('numField2');
+var resultField = document.getElementById('resultField');
 
-numField1.value = 'Hey Bob!';
+numField1.value = "TEST"; 
+resultField.innerText = "TEST2";
