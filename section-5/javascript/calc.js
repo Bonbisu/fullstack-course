4853448a46d57a83e6330c8e0a1649ef948df729
bomber <fh.costa@yahoo.com.br>
2018-06-18 23:07:37 -0300

    section 5-45
    getting values from field and printing on console

--- section-5/javascript/calc.js
+++ section-5/javascript/calc.js
@@ -4,5 +4,20 @@ var resultField = document.getElementById('resultField');
 var form = document.getElementById('xIsWhatPercentageOfY');
 
 form.addEventListener('submit', function() {
-    alert("We clickeyclacked something!");
+    var x = numField1.value;
+    var y = numField2.value;
+    
+    console.log(x);
+    console.log(y);
 });
+
+
+//Another way to call this function ahead, chaining functions
+//
+//document.getElementById('xIsWhatPercentageOfY').addEventListener('submit', function() {
+//    alert("We clickeyclacked something!");
+//});
+
+
+
+
